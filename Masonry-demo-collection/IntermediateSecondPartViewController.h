729a344a5600a3debca33a66d7129d91cf3c9cd5
IOF－IOS2 <593686620@qq.com>
2015-09-09 16:01:32 +0800

    增加文件

---
+++ Masonry-demo-collection/IntermediateSecondPartViewController.h
@@ -0,0 +1,13 @@
+//
+//  IntermediateSecondPartViewController.h
+//  Masonry-demo-collection
+//
+//  Created by IOF－IOS2 on 15/9/9.
+//  Copyright (c) 2015年 IOF－IOS2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IntermediateSecondPartViewController : UIViewController
+
+@end
---
+++ Masonry-demo-collection/IntermediateSecondPartViewController.m
@@ -0,0 +1,26 @@
+//
+//  IntermediateSecondPartViewController.m
+//  Masonry-demo-collection
+//
+//  Created by IOF－IOS2 on 15/9/9.
+//  Copyright (c) 2015年 IOF－IOS2. All rights reserved.
+//
+
+#import "IntermediateSecondPartViewController.h"
+
+@interface IntermediateSecondPartViewController ()
+
+@end
+
+@implementation IntermediateSecondPartViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+}
+
+
+@end
